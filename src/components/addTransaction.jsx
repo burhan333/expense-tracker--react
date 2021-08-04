159d45665031a5ebb985a24fba8644afc5e72880
burhan <em.burhan@yahoo.com>
2021-08-05 04:59:53 +0500

    styling

--- src/components/addTransaction.jsx
+++ src/components/addTransaction.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const AddTransaction = ({handleChange, handleValue}) => {
     return(
         <React.Fragment>
+            <h3>Add New Transaction</h3>
             <div className="Text">
                 <p>text</p>
                 <input onChange={(e) => handleChange(e, 'text')} type="text" />
@@ -11,7 +12,7 @@ const AddTransaction = ({handleChange, handleValue}) => {
                 <p>Amount</p>
                 <input onChange={(e) => handleChange(e, 'amount')} type="number" />
             </div>
-            <button onClick={handleValue}>add transaction</button>
+            <button onClick={handleValue} className="btn">add transaction</button>
         </React.Fragment>
     )
 }
