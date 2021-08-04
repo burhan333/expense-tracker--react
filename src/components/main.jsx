fcd0dfe8c2e539c86f8918ac311ec11e01b99ab1
burhan <em.burhan@yahoo.com>
2021-08-05 02:28:49 +0500

    functionality complete

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -25,20 +25,23 @@ const Main = () => {
         })
     }
 
-
-    const amounts = list.map(item => parseInt(item.amount))
+    const amount= list.map(item => parseFloat(item.amount))
     
-    const income = amounts.filter(item => item > 0).reduce((acc, value) => (acc + value), 0);
+    const income = amount.filter(item => item > 0).reduce((acc, value) => (acc + value), 0).toFixed(2);
+
+    const expense = (amount.filter(item => item < 0).reduce((acc, value) => (acc + value), 0) * -1).toFixed(2);
+
+    const balance = (income - expense).toFixed(2);
 
     return (
         <React.Fragment>
             <h1>Expense Tracker</h1>
             <h3>Your Balance</h3>
-            <h1>${}</h1>
+            <h1>${balance}</h1>
             <h3>income</h3>
             <h3>{income}</h3>
             <h3>expense</h3>
-            <h3>{amounts}</h3>
+            <h3>{expense}</h3>
             <div className="history">
                 <h3>History</h3>
                 <ul>
